8d582a4154081739fdee648fc4f731036eae7f1b
Lewis <mojicalewis@gmail.com>
2025-07-19 13:14:01 -0400

    refactor: rename doctype from Factura proforma to Factura Proforma

--- financed_sales/financed_sales/doctype/factura_profoma/factura_profoma.js
+++ financed_sales/financed_sales/doctype/factura_profoma/factura_profoma.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2025, Lewis Mojica and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Factura profoma", {
+// frappe.ui.form.on("Factura Profoma", {
 // 	refresh(frm) {
 
 // 	},
--- financed_sales/financed_sales/doctype/factura_profoma/test_factura_profoma.py
+++ financed_sales/financed_sales/doctype/factura_profoma/test_factura_profoma.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestFacturaprofoma(FrappeTestCase):
+class TestFacturaProfoma(FrappeTestCase):
 	pass
