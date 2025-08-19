2c3b2a044f7ae19bedd2b39f7b7b9f6a9578c3a3
Lewis <mojicalewis@gmail.com>
2025-08-19 00:16:44 -0400

    feat: populate custom_insterests field when creating (credit) Sales Invoice.

--- financed_sales/financed_sales/create_credit_invoice.py
+++ financed_sales/financed_sales/create_credit_invoice.py
@@ -4,23 +4,13 @@ from frappe import _
 def main(doc,method):
 	if doc.workflow_state == 'Approved':
 		invoice = frappe.new_doc('Sales Invoice')
-		interest_item = frappe.get_doc('Item', 'INTEREST')
-		
 		quotation = frappe.get_doc('Quotation',doc.quotation)
+
 		for item in quotation.items:
 			invoice.append('items',{
 				'item_code': item.item_code,
 				'qty': item.qty,
 			})
-		invoice.append('items',{
-					'item_code': 'INTEREST',
-					'item_name': _('Interest'),
-					'qty': 1,
-					'rate': doc.interests, 
-					'uom': interest_item.stock_uom,
-					'conversion_factor': 1,
-					
-				})
 
 		invoice.custom_is_credit_invoice = True
 		invoice.due_date = doc.installments[-1].due_date
@@ -31,6 +21,7 @@ def main(doc,method):
 		invoice.selling_price_list = quotation.selling_price_list
 		invoice.price_list_currency = quotation.price_list_currency
 		invoice.plc_conversion_rate = quotation.plc_conversion_rate
+		invoice.custom_interests = doc.interests
 		
 
 	invoice.insert()
