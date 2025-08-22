89eb6b16e65be0594c4f14d225f82a2985fc2977
Lewis <mojicalewis@gmail.com>
2025-08-22 17:44:02 -0400

    feat(Payment Plan): Add table to keep track of all the payment related to the Payment Plan
    - The button to create payment now calls the corresponding api method

--- financed_sales/financed_sales/doctype/payment_plan/payment_plan.js
+++ financed_sales/financed_sales/doctype/payment_plan/payment_plan.js
@@ -6,3 +6,20 @@
 
 // 	},
 // });
+frappe.ui.form.on('Payment Plan', {
+    gen_installment_payment: function(frm) {
+        frappe.call({
+            method: "financed_sales.financed_sales.api.create_payment_from_pay_plan",
+            args: { payment_plan_name: frm.doc.name, },
+            callback: function(response) {
+                if (response.message) {
+                    // Open the payment entry form
+                    var doclist = frappe.model.sync(response.message);
+                    frappe.set_route("Form", doclist[0].doctype, doclist[0].name);
+                } else {
+                    frappe.msgprint("Error creating payment entry");
+                }
+            }
+        });
+    }
+});
