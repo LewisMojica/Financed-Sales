2b095aa2134f4716e21833d3f414791ddaffa851
Lewis <mojicalewis@gmail.com>
2025-08-22 12:37:53 -0400

    fix: use make_sales_invoice method so Sales Invoice is linked to the Sales order or the Finance Application

--- financed_sales/financed_sales/create_docs_on_approval.py
+++ financed_sales/financed_sales/create_docs_on_approval.py
@@ -51,39 +51,25 @@ def on_approval(doc):
 	
 def create_credit_inv(doc, submit = True):
 	settings = frappe.get_single('Financed Sales Settings')
-	invoice = frappe.new_doc('Sales Invoice')
+	invoice = make_sales_invoice(doc.sales_order)
 	quotation = frappe.get_doc('Quotation',doc.quotation)
-
-	for item in quotation.items:
-		invoice.append('items',{
-			'item_code': item.item_code,
-			'qty': item.qty,
-		})
 
 	invoice.custom_is_credit_invoice = True
 	invoice.due_date = doc.installments[-1].due_date
-	invoice.customer = quotation.party_name
-	invoice.company = quotation.company
-	invoice.currency = quotation.currency
-	invoice.conversion_rate = quotation.conversion_rate
-	invoice.selling_price_list = quotation.selling_price_list
-	invoice.price_list_currency = quotation.price_list_currency
-	invoice.plc_conversion_rate = quotation.plc_conversion_rate
 	account = settings.interests_account #account for interest
-	for tax in quotation.taxes:
-		invoice.append('taxes',tax)
+
 	invoice.append('taxes', {
 		'charge_type': 'Actual',
 		'account_head': account,
 		'description': 'Intereses',
 		'tax_amount': doc.interests,
 	})
-	
 
 	invoice.insert()
 	if submit:
 		invoice.submit()
 	return invoice.name
+
 def create_payment_plan(doc, submit = True):
 	plan = frappe.new_doc('Payment Plan')
 	plan.finance_application = doc.name
