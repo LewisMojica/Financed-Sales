a2dd2c5b451ca974faa7b74026ab322e167787fa
Lewis <mojicalewis@gmail.com>
2025-08-22 16:35:20 -0400

    feat: add update logic for Finance application field relating down payment

--- financed_sales/financed_sales/update_payments.py
+++ financed_sales/financed_sales/update_payments.py
@@ -23,9 +23,18 @@ def main(pe, method):
 	if (fa.workflow_state, ref.reference_doctype) not in valid_state_and_ref_combinations:
 		frappe.throw(f'Wrong ref doctype for {fa.workflow_state} Finance Application')
 
+	# Add payment to table
 	fa.append('down_payments',{
 		'payment_entry': pe.name,
 		'amount': pe.paid_amount,
 		'date': pe.posting_date,
 	})
+	#update down payment fields
+	paid_down_payment = 0
+	for payment in fa.down_payments:
+		paid_down_payment += payment.amount
+	
+	fa.paid_down_payment = paid_down_payment
+	fa.paid_down_payment_percent = 100*paid_down_payment/fa.down_payment
+	fa.pending_down_payment = fa.down_payment - paid_down_payment
 	fa.save()
