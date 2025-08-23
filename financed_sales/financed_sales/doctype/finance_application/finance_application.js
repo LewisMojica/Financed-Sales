562b4a865434b0c02c1f8aa191614d50c79bc49d
Lewis <mojicalewis@gmail.com>
2025-08-23 11:52:36 -0400

    feat: use simple dialog to create down payment entries from a Finance Application

--- financed_sales/financed_sales/doctype/finance_application/finance_application.js
+++ financed_sales/financed_sales/doctype/finance_application/finance_application.js
@@ -8,18 +8,68 @@
 // });
 frappe.ui.form.on('Finance Application', {
     gen_down_payment: function(frm) {
-        frappe.call({
-            method: "financed_sales.financed_sales.api.create_down_payment_from_fin_app",
-            args: { fin_app_name: frm.doc.name, },
-            callback: function(response) {
-                if (response.message) {
-                    // Open the payment entry form
-                    var doclist = frappe.model.sync(response.message);
-                    frappe.set_route("Form", doclist[0].doctype, doclist[0].name);
-                } else {
-                    frappe.msgprint("Error creating payment entry");
-                }
-            }
-        });
+		dialog = get_payment_dialog(frm.doc.name)
+		dialog.show()
+		dialog.hide()
+		
     }
 });
+
+
+
+frappe.ui.form.on('Payment Plan', {
+         gen_installment_payment: function(frm) {
+                        d = get_payment_dialog(frm.doc.name);
+                        d.show();
+                        d.hide();
+         },
+        gen_down_payment: function(frm){
+                d = get_payment_dialog(frm.doc.name);
+                d.show();
+                d.hide();
+        }
+});
+
+function get_payment_dialog(fa_name){
+return new frappe.ui.Dialog({
+        title: 'Payment details',
+        fields: [
+                        {
+                                label: 'Amount to pay',
+                                fieldname: 'paid_amount',
+                                fieldtype: 'Currency',
+                        },
+                        {
+                                label: 'Payment method',
+                                fieldname: 'mode_of_payment',
+                                fieldtype: 'Link',
+                                options: 'Mode of Payment',
+                        },
+        ],
+        size: 'small', // small, large, extra-large
+        primary_action_label: 'Submit payment',
+        primary_action(values) {
+                frappe.call({
+                        method: "financed_sales.financed_sales.api.create_payment_entry_from_finance_application",
+                        args: {
+									finance_application_name:fa_name,
+									paid_amount: values.paid_amount,
+									mode_of_payment: values.mode_of_payment,
+									submit: true
+								},
+                        callback: function(response) {
+                                if (response.message) {
+                                        frappe.show_alert({
+                  message: __('Payment Entry created successfully'),
+                  indicator: 'green'
+               });
+                                        frappe.set_route('Form', 'Payment Entry', response.message);
+                                } else {
+                                        frappe.msgprint("Error creating payment entry");
+                                }
+                        }
+                });
+         }
+});
+
+}
