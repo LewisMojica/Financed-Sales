868ef38293af48e95391663c3b620687dab182c4
Lewis <mojicalewis@gmail.com>
2025-08-27 13:43:58 -0400

    feat: udpate state when a payment plan is submited

--- financed_sales/financed_sales/doctype/payment_plan/payment_plan.py
+++ financed_sales/financed_sales/doctype/payment_plan/payment_plan.py
@@ -1,9 +1,14 @@
 # Copyright (c) 2025, Lewis Mojica and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from financed_sales.financed_sales.update_payments import auto_alloc_payments, apply_installments_state
 
 
 class PaymentPlan(Document):
-	pass
+	def before_submit(self):
+		print('~~~~~~~~~~~~~~~~~~~lsubmitted!~~~~~~~~~~~~~~~~~~\n\n~~~~~~~~~~~~~~~~~~~~~~~~~')
+		state = auto_alloc_payments(self.down_payment_amount, self.installments, self.payment_refs)
+		apply_installments_state(self, state)
+		print(state)
