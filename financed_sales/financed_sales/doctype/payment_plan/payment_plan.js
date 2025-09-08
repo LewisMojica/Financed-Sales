1ef73c202e2861063b31d351ee02d5b131f8fa62
LewisMojica <mojicalewis@gmail.com>
2025-09-07 23:21:24 -0400

    feat: add reference fields to Payment Plan dialog
    - Add reference_number and reference_date fields to Payment Plan payment dialog
    - Implement conditional visibility for Wire Transfer and Credit Card payments
    - Update API call to pass reference parameters when needed
    - Maintain consistency with Finance Application dialog functionality

--- financed_sales/financed_sales/doctype/payment_plan/payment_plan.js
+++ financed_sales/financed_sales/doctype/payment_plan/payment_plan.js
@@ -8,9 +8,9 @@
 // });
 frappe.ui.form.on('Payment Plan', {
 	 gen_installment_payment: function(frm) {
-			d = get_payment_dialog(frm.doc.name); 
-			d.show();
-			d.hide();
+		d = get_payment_dialog(frm.doc.name); 
+		d.show();
+		d.hide();
 	 },
 	gen_down_payment: function(frm){
 		d = get_payment_dialog(frm.doc.name);
@@ -33,14 +33,51 @@ return new frappe.ui.Dialog({
 				fieldname: 'mode_of_payment',
 				fieldtype: 'Link',
 				options: 'Mode of Payment',
+				onchange: function() {
+					const mode_of_payment = this.get_value();
+					const dialog = window.cur_dialog;
+				   
+					if (mode_of_payment === 'Wire Transfer' || mode_of_payment === 'Credit Card') {
+						dialog.set_df_property('reference_number', 'hidden', false);
+						dialog.set_df_property('reference_date', 'hidden', false);
+					} else {
+						dialog.set_df_property('reference_number', 'hidden', true);
+						dialog.set_df_property('reference_date', 'hidden', true);
+					}
+				}
+			},
+			{
+				label: 'Reference Number',
+				fieldname: 'reference_number',
+				fieldtype: 'Data',
+				hidden: 1,
+			},
+			{
+				label: 'Reference Date',
+				fieldname: 'reference_date',
+				fieldtype: 'Date',
+				default: frappe.datetime.get_today(),
+				hidden: 1,
 			},
 	],
 	size: 'small', // small, large, extra-large 
 	primary_action_label: 'Submit payment',
 	primary_action(values) {
+		const args = {
+			payment_plan_name: pp_name,
+			paid_amount: values.paid_amount,
+			mode_of_payment: values.mode_of_payment,
+			submit: true
+		};
+		
+		if (values.mode_of_payment === 'Wire Transfer' || values.mode_of_payment === 'Credit Card') {
+			args.reference_number = values.reference_number;
+			args.reference_date = values.reference_date;
+		}
+		
 		frappe.call({
 			method: "financed_sales.financed_sales.api.create_payment_entry_from_payment_plan",
-			args: { payment_plan_name:pp_name, paid_amount: values.paid_amount, mode_of_payment: values.mode_of_payment, submit: true},
+			args: args,
 			callback: function(response) {
 				if (response.message) {
 					frappe.show_alert({                                        				
