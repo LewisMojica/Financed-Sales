05e5d416d3fca946a6cb757dd9e72935f69d69bc
LewisMojica <mojicalewis@gmail.com>
2025-09-11 13:13:21 -0400

    fix: update backend to use payment method type for reference fields

--- financed_sales/financed_sales/api.py
+++ financed_sales/financed_sales/api.py
@@ -134,10 +134,11 @@ def create_payment_entry(doc, paid_amount, mode_of_payment, submit = False, refe
 	pe.paid_to = account
 	pe.custom_is_finance_payment = 1
 	
-	# Set reference number and date for Wire Transfer and Credit Card
-	if mode_of_payment in ["Wire Transfer", "Credit Card"] and reference_number:
+	# Set reference number and date for Bank type payments
+	mode_of_payment_type = frappe.db.get_value('Mode of Payment', mode_of_payment, 'type')
+	if mode_of_payment_type == 'Bank' and reference_number:
 		pe.reference_no = reference_number
-	if mode_of_payment in ["Wire Transfer", "Credit Card"] and reference_date:
+	if mode_of_payment_type == 'Bank' and reference_date:
 		pe.reference_date = reference_date
 	
 	pe.save()
